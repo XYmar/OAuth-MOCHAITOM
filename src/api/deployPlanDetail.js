f7fd77a3ddcd42aced7081df03c9121f951e8c20
XYmar <xqingliuwu@163.com>
2018-06-28 10:26:24 +0800

    实现部署设计详情的查询功能及搜索功能

--- src/api/deployPlanDetail.js
+++ src/api/deployPlanDetail.js
@@ -3,10 +3,6 @@ import request from '../utils/request'
 export function bindDetailList(deployplanId) {
   return request({
     url: 'deploymentdesigns/' + deployplanId + '/deploymentdesigndetails',
-    method: 'get',
-    auth: {
-      username: 'admin',
-      password: 'admin'
-    }
+    method: 'get'
   })
 }
