b2755c7c5f4c2adeb5391b76e33ecb7626becde4
XYmar <xqingliuwu@163.com>
2018-07-19 16:18:51 +0800

    token过期时回到登录页面

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -5,11 +5,15 @@ const TokenKey = 'Admin-Token'
 export function getToken() {
   return Cookies.get(TokenKey)
 }
-
 export function setToken(token) {
   return Cookies.set(TokenKey, token)
 }
-
+export function getRefreshToken() {
+  return Cookies.get('RefreshTokenKey')
+}
+export function setRefreshToken(token) {
+  return Cookies.set('RefreshTokenKey', token)
+}
 export function removeToken() {
   return Cookies.remove(TokenKey)
 }
@@ -38,9 +42,21 @@ export function setExpire(timestamp) {
 }
 
 export function getExpire(timestamp) {
-  return Cookies.get('timestamp', timestamp)
+  return Cookies.get('timestamp')
 }
 
 export function removeExpire(timestamp) {
   return Cookies.remove('timestamp')
+}
+
+export function setExpire2(reTimestamp) {
+  return Cookies.set('reTimestamp', reTimestamp)
+}
+
+export function getExpire2(reTimestamp) {
+  return Cookies.get('reTimestamp')
+}
+
+export function removeExpire2(reTimestamp) {
+  return Cookies.remove('reTimestamp')
 }
